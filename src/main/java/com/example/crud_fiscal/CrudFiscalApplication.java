43f34fdc922c60ba15a5c54b58bfac837f2e03bd
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-05-25 12:46:55 -0500

    Add init files

---
+++ src/main/java/com/example/crud_fiscal/CrudFiscalApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_fiscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudFiscalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFiscalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crud_fiscal/CrudFiscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_fiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFiscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
