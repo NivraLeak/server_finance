2c5fdb5c3d8a2dfa17890e27acfafa52617446f7
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-05-25 14:54:17 -0500

    Add repositories

--- src/main/java/com/example/crud_fiscal/domain/repository/CategoryRepository.java
+++ src/main/java/com/example/crud_fiscal/domain/repository/CategoryRepository.java
@@ -1,11 +1,9 @@
 package com.example.crud_fiscal.domain.repository;
 
-import com.example.crud_fiscal.domain.entities.FiscalPosition;
+import com.example.crud_fiscal.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface FiscalPositionRepository extends JpaRepository<FiscalPosition, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
