125f0edd4214e51315e0a2f7155778672ccdc7f8
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-05-25 15:58:14 -0500

    Add implement methods of category service

--- src/main/java/com/example/crud_fiscal/domain/service/impl/CategoryPositionServiceImpl.java
+++ src/main/java/com/example/crud_fiscal/domain/service/impl/CategoryPositionServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.crud_fiscal.domain.service.impl;
 
-public class CategoryPositionServiceImpl {
+import com.example.crud_fiscal.domain.repository.CategoryRepository;
+import com.example.crud_fiscal.domain.service.CategoryService;
+import com.example.crud_fiscal.persistence.dto.entities.CategoryDto;
+import com.example.crud_fiscal.persistence.dto.entities.SaveCategoryDto;
+import com.example.crud_fiscal.persistence.dto.error.ErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryPositionServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryPositionServiceImpl(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public CategoryDto saveCategory(SaveCategoryDto createCategory) throws ErrorException {
+        return null;
+    }
+
+    @Override
+    public CategoryDto updateCategoryById(Integer idCategory, SaveCategoryDto saveCategoryDto) throws ErrorException {
+        return null;
+    }
+
+    @Override
+    public List<CategoryDto> listAllCategory() throws ErrorException {
+        return null;
+    }
+
+    @Override
+    public CategoryDto getCategoryById(Integer categoryId) throws ErrorException {
+        return null;
+    }
+
+    @Override
+    public String deleteCategoryById(Integer categoryId) throws ErrorException {
+        return null;
+    }
 }
