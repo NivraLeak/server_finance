32201904816e78248baec97b44105d59a9d212ee
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-05-25 13:46:53 -0500

    edit swagger cofig

--- src/main/java/com/example/crud_fiscal/config/SwaggerConfig.java
+++ src/main/java/com/example/crud_fiscal/config/SwaggerConfig.java
@@ -17,14 +17,9 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
-    }
-
-    private ApiKey apiKey(){
-        return new ApiKey("JWT", "Authorization", "header");
     }
 }
